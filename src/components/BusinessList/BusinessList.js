3ff896ac6775e87f7f097521a2abffea154390b2
tspat <scott@tumble.to>
2020-06-30 11:52:44 -0700

    Restructured BusinessList Component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './BusinessList.css'
-import Business from '../components/Business/Business'
+import Business from '../Business/Business'
 
 class BusinessList extends React.Component {
 
